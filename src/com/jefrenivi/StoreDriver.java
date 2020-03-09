7a866aa8b6325d55400c1ec7edaef16c0bf45f3e
liftedplane <liftedplane@gmail.com>
2020-03-09 11:03:21 -0700

    force branch to remote repository, because git is trash.

--- src/com/jefrenivi/StoreDriver.java
+++ src/com/jefrenivi/StoreDriver.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+//testing
 public class StoreDriver {
 	Scanner scan = new Scanner(System.in);
 	private static JDBC sql;
