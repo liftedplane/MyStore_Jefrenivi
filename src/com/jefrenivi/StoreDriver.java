580317683e537b7a68d1e48a570a0653053caa95
liftedplane <liftedplane@gmail.com>
2020-03-09 14:10:42 -0700

    Revert "Add Shipper/Supplier methods"
    This reverts commit 571fd889de92addd5c6cec888799a9886d7c38d7.

--- src/com/jefrenivi/StoreDriver.java
+++ src/com/jefrenivi/StoreDriver.java
@@ -270,18 +270,19 @@ public class StoreDriver {
 		}
 	
 	//_________Shippers and Supplies Menu_________
-	private void shippersAndSuppliers() throws SQLException {
+	//need to create method body for Ships and Supplies
+	private void shippersAndSuppliers() {
 		System.out.println("********** SHIPPERS AND SUPPLIERS MENU **********");
 		System.out.println("1. View Shippers\n2. View Suppliers");
 		System.out.println("Enter Option Number");
 		String opt = scan.nextLine();
 		switch (opt) {
 		case "1":
-			viewShippers();
+//			viewShippers();
 			break;
-	
+			
 		case "2":
-			viewSuppliers();
+//			viewSuppliers();
 			break;
 			
 		default:
@@ -289,20 +290,8 @@ public class StoreDriver {
 			shippersAndSuppliers();
 		}
 	}
-	
-	private void viewShippers() throws SQLException {
-		System.out.println("*******Viewing All Shippers*******");
-		ResultSet rs = sql.getAllShippers();
-		displayResults(rs);
-	}
-	
-	private void viewSuppliers() throws SQLException {
-		System.out.println("*******Viewing All Suppliers*******");
-		ResultSet rs = sql.getAllSuppliers();
-		displayResults(rs);
-	}
-	
-	
+
+	// displays results when called
 	private void displayResults(ResultSet rs) throws SQLException {
 
 		ResultSetMetaData rsmd = rs.getMetaData();
